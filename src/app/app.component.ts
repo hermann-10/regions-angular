05109c998ea3deb3abf41912eccf36d388d22445
hermannmanuel <hermann.manuel>
2024-02-22 14:54:37 +0100

    Turning a text field into an Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { RegionsService } from './services/regions.service';
 import { CommonModule } from '@angular/common';
 import { Observable, Subscription, tap } from 'rxjs';
 import Region from './interfaces/region';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -19,6 +19,10 @@ export class AppComponent implements OnInit, OnDestroy {
     this.subscription = this.regionService
     .getRegions()
     .pipe(tap((data => this.regionsSig.set(data))))
+    .subscribe();
+
+    this.departmentSubscription = this.textControl.valueChanges
+      .pipe(tap((data) => console.log({ departmentFilter: data } )))
     .subscribe();
   }
 
@@ -32,6 +36,8 @@ export class AppComponent implements OnInit, OnDestroy {
   regionService = inject(RegionsService);
   selectedRegionSig = this.regionService.selectedRegionSig;
   subscription!: Subscription;
+  departmentSubscription!: Subscription;
+
   regionDepartmentsSig = this.regionService.regionDepartmentsSig;
   filterBySig = signal<string>('');
 
@@ -50,6 +56,7 @@ export class AppComponent implements OnInit, OnDestroy {
     console.log({ filteredRegions: this.regions });
   });
 
+  textControl = new FormControl();
   // resetRegions(){
   //   this.regions = this.regionsSig();
   // }
