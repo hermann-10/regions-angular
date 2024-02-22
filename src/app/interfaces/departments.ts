a0c849cecfb34d61564059f90140e9793af32abb
hermannmanuel <hermann.manuel>
2024-02-22 13:42:47 +0100

    Making an HTTP request from effect()

---
+++ src/app/interfaces/departments.ts
@@ -0,0 +1,5 @@
+export default interface Department{
+  nom: string;
+  code: string;
+  codeRegion: string;
+}
--- src/app/services/regions.service.ts
+++ src/app/services/regions.service.ts
@@ -2,16 +2,27 @@ import { Injectable, effect, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map, tap } from 'rxjs';
 import Region, { RegionDTO } from './../interfaces/region';
+import Department from '../interfaces/departments';
 ;
 @Injectable({
   providedIn: 'root'
 })
 export class RegionsService {
 
-  private REGIONS_URL ='https://geo.api.gouv.fr/regions?fields=nom,code';
+  private REGIONS_URL = 'https://geo.api.gouv.fr/regions?fields=nom,code';
+
+  // full departement url should be https://geo.api.gouv.fr/regions/53/departements
+  private DEPARTMENTS_URL = 'https://geo.api.gouv.fr/regions';
+  regionDepartmentsSig = signal<Department[]>([])
+
   selectedRegionSig = signal<Region>({ Code: '-1', Name: ''});
   private departementsEffect = effect(()=> {
-    console.log(this.selectedRegionSig())
+    console.log(this.selectedRegionSig());
+    if (this.selectedRegionSig().Code === '-1'){
+      return;
+    }
+    this.http.get<Department[]>(`${this.DEPARTMENTS_URL}/${this.selectedRegionSig().Code}/departements`).pipe(tap(data => console.log({ departments: data })))
+    .subscribe(departments => this.regionDepartmentsSig.set(departments))
   })
   http = inject(HttpClient)
 
